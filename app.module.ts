93a8be63f2fc71d6fac6364c252d0043ded50a8b
Dillip kumar sahoo <68984787+Dillip-error@users.noreply.github.com>
2020-11-09 08:54:33 +0530

    material-dynamic-table

--- app.module.ts
+++ app.module.ts
@@ -1,44 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TableComponent } from './Components/table.component';
-import { TableModule } from './Module/table/table.module';
-import { HttpClientModule } from '@angular/common/http';
-import { OwnerModule } from './owner/owner.module';
-import { RouterModule } from '@angular/router';
-import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
-import { HomeComponent } from './home/home.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material/material/material.module';
+import { EmployeeService } from './shared/employee.service';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { StyleBindingComponent } from './Test1/Components/style-binding/style-binding.component';
+import { ClassBindingComponent } from './Test1/Components/class-binding/class-binding.component';
+import { DatePipePipe } from './date-pipe.pipe';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import { SpinnerViewComponent } from './spinner-view/spinner-view.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { DynamicTableEditComponent } from './dynamic-table-edit/dynamic-table-edit.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
-RouterModule.forRoot([
-  { path: 'home', component: HomeComponent },
-  { path: 'owner', loadChildren: () => import('./owner/owner.module').then(m => m.OwnerModule) },
-  { path: '404', component : NotFoundComponentComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/404', pathMatch: 'full'}
-])
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent,
-    NotFoundComponentComponent,
-    HomeComponent,
-    SideNavComponent,
-    HeaderComponent
+    EmployeeComponent,
+    StyleBindingComponent,
+    ClassBindingComponent,
+    SpinnerViewComponent,
+    DynamicTableEditComponent,
+
+   // DatePipePipe
     
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
-    TableModule,
-    HttpClientModule,
-    OwnerModule
+    MaterialModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDialogModule, 
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+    
+    
+    
   ],
   providers: [],
-  bootstrap: [TableComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
